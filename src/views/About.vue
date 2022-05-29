ae64209d7c80439e50cdd35955aaafedc131d7d9
Cody Volz <quantonly.dev@gmail.com>
2022-05-29 12:38:04 +0200

    Home, About, Contact design

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,86 @@
 <template>
   <main-layout>
     <div class="about darkmode-animation mb-auto">
-      <p>About</p>
+      <section class="darkmode-animation dark:bg-darkmode-medium bg-white py-8">
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+            <div class="w-full lg:w-6/12 px-4 mx-auto text-center">
+              <h2 class="darkmode-animation text-5xl font-semibold dark:text-white">About</h2>
+              <p class="darkmode-animation dark:text-white text-lg leading-relaxed mt-4 mb-4 text-blueGray-400">MoonNetic is a team of five young, driven students from Cloud & Cyber Security. With the knowledge we acquired during our first two years of our course, we have succeeded in setting up a hosting platform.</p>
+              <p class="darkmode-animation dark:text-white text-lg leading-relaxed mt-4 mb-4 text-blueGray-400">Our name originated from our pursuit of reliability of our platform. We find it important that you website will be online as much as possible. We got our name from NASA’s moon landing, as it only used 8kb of RAM to get there. That’s how we got to the idea that our hosting service should be efficient and reliable. Our slogan “Hosting till the moon and back” also came from this.</p>
+            </div>
+            <h1 class="darkmode-animation dark:text-white w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+            Our Team
+          </h1>
+          <div class="w-full mb-4">
+            <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+          </div>
+          <div class="w-full p-6 flex">
+            <div class="darkmode-animation dark:bg-darkmode-light flex flex-1 bg-white rounded-3xl overflow-hidden shadow">
+              <img src="../assets/images/els.jpg" alt="" class="h-72">
+              <div>
+                <div class="mt-5 darkmode-animation dark:text-white font-bold text-xl text-gray-800 px-6">
+                  Els Bresseleers
+                </div>
+                <p class="darkmode-animation dark:text-white text-gray-800 text-base px-6 mb-5">
+                  Our first group member is Els Bresseleers. Her hobby is Zumba, and her professional interest is subjects like Cyber Security. She also has been active since 2012 at her local theatre group. This evolved her social skills tremendously and got her to where she is today. With a diploma in Baking, Pastries and Nursing, she is the very experienced. She gained a lot of knowledge through orthopedagogy as well, which provides her with the necessary people skills. As a future job she would love to work at the RCCU (Regional Cybercrime Unit), FCCU (Federal Cybercrime Unit) or the army as an IT-specialist.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="w-full p-6 flex">
+            <div class="darkmode-animation dark:bg-darkmode-light flex flex-1 bg-white rounded-3xl overflow-hidden shadow flex-row-reverse">
+              <img src="../assets/images/casper.png" alt="" class="h-72">
+              <div>
+                <div class="mt-5 darkmode-animation dark:text-white font-bold text-xl text-gray-800 px-6">
+                  Casper Lemmens
+                </div>
+                <p class="darkmode-animation dark:text-white text-gray-800 text-base px-6 mb-5">
+                  Casper Lemmens started his education at the Saint Jozef middle school in technical sciences. After 5 years, he changed his course to something more IT related. This is how he started with accounting and computer sciences where he graduated in. His professional interests lay in cybersecurity, but in his leisure, you can find him in the gym, gaming, or cooking. He also enjoys taking a walk with his dog. For the future, he would like to work at PWC Belgium. This consulting company has a mayor focus in cybersecurity with aligns with his interests. Here he hopes to acquire a place in the internal security team of the company.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="w-full p-6 flex">
+            <div class="darkmode-animation dark:bg-darkmode-light flex flex-1 bg-white rounded-3xl overflow-hidden shadow">
+              <img src="../assets/images/jentel.jpg" alt="" class="h-72">
+              <div>
+                <div class="mt-5 darkmode-animation dark:text-white font-bold text-xl text-gray-800 px-6">
+                  Jentel Liekens
+                </div>
+                <p class="darkmode-animation dark:text-white text-gray-800 text-base px-6 mb-5">
+                  Jentel Liekens started his educational career with four years of pursuing the sciences in the Saint Ursula middle school. He started with two and a half years in STEM, followed by the technical sciences until his fourth year. The next step in his career was joining the technical schools in Mechelen, where he took the first steps towards IT. This is also where he acquired his diploma in industrial IT. His professional interests are in cybersecurity, but he likes to relax by playing darts, photography en soccer. Which is why you can always find him back at the stadium encouraging his favorite team.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="w-full p-6 flex">
+            <div class="darkmode-animation dark:bg-darkmode-light flex flex-1 bg-white rounded-3xl overflow-hidden shadow flex-row-reverse">
+              <img src="../assets/images/jentel.jpg" alt="" class="h-72">
+              <div>
+                <div class="mt-5 darkmode-animation dark:text-white font-bold text-xl text-gray-800 px-6">
+                  Tim Verbecque
+                </div>
+                <p class="darkmode-animation dark:text-white text-gray-800 text-base px-6 mb-5">
+                  Our fourth member is Tim Verbecque. He started middle school at GITO Nijlen. Here he studied electromechanics for six years. Afterwards he chose for a career switch and started his studies in IT. He had developed an interest in IT during middle school anyways. This is why he started in the IT Factory at Thomas More Geel. Professionally he is very interested in penetration testing. This is also his plan, becoming a penetration tester or cybersecurity consultant. During his leisure, he likes to go to the gym, listen to music or watch some movies. Gaming and running are also some of his hobbies.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="w-full p-6 flex">
+            <div class="darkmode-animation dark:bg-darkmode-light flex flex-1 bg-white rounded-3xl overflow-hidden shadow">
+              <img src="../assets/images/cody.jpg" alt="" class="h-72">
+              <div>
+                <div class="mt-5 darkmode-animation dark:text-white font-bold text-xl text-gray-800 px-6">
+                  Cody Volz
+                </div>
+                <p class="darkmode-animation dark:text-white text-gray-800 text-base px-6 mb-5">
+                  Cody Volz is our team member with the most experience out of us all. Having already completed a course at the IT Factory of Thomas More, he has acquired a diploma in computer science, specializing in Application Development. During middle school however, he was more focused on the sciences. The first three years he started at SH Dilsen. His fourth year he completed at Saint Augustinus Bree and his last 2 years he studied IT at KTA Hasselt, more specifically: IT management. His professional interests are Application Development and cyber security, and his leisure is spent on music and movies. He also enjoys creating small games in Unity.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </main-layout>
 </template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,50 @@
 <template>
   <main-layout>
     <div class="contact darkmode-animation mb-auto">
-      <p>Contact</p>
+      <section class="pb-20 relative block bg-blueGray-800">
+        <div class="container mx-auto px-4 lg:pt-24 lg:pb-64">
+          <div class="flex flex-wrap text-center justify-center">
+            <div class="w-full lg:w-6/12 px-4">
+              <h2 class="darkmode-animation text-4xl font-semibold dark:text-white">Contact</h2>
+              <p class="darkmode-animation dark:text-white text-lg leading-relaxed mt-4 mb-4 text-blueGray-400">
+                If you have any questions, complaints or feedback, you can always let us know through our E-mail or fill in the form below. We will try our best to give you an answer as soon as possible.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="relative block pt-24 lg:pt-0 mb-16">
+        <div class="container mx-auto px-4">
+          <div class="flex flex-wrap justify-center lg:-mt-64 -mt-48">
+            <div class="w-full lg:w-6/12 px-4">
+              <div class="darkmode-animation relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg dark:bg-darkmode-light">
+                <div class="flex-auto p-5 lg:p-10">
+                  <h4 class="text-2xl font-semibold darkmode-animation dark:text-white">Fill in this form</h4>
+                  <p class="leading-relaxed mt-1 mb-4 text-blueGray-500 darkmode-animation dark:text-white">
+                    And we will get back to you in 24 hours.
+                  </p>
+                  <form action="mailto:cody.volz@hotmail.com" method="post" enctype="text/plain">
+                    <div class="relative w-full mb-3 mt-8">
+                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2 darkmode-animation dark:text-white" for="name">Full Name</label><input type="text" name="name" id="name" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Full Name">
+                    </div>
+                    <div class="relative w-full mb-3">
+                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2 darkmode-animation dark:text-white" for="email">Email</label><input type="email" name="email" id="email" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" placeholder="Email">
+                    </div>
+                    <div class="relative w-full mb-3">
+                      <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2 darkmode-animation dark:text-white" for="message">Message</label><textarea name="message" id="message" rows="4" cols="80" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full" placeholder="Type a message..."></textarea>
+                    </div>
+                    <div class="text-center mt-6">
+                      <button class="darkmode-animation dark:bg-white dark:text-black active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="submit">
+                        Send Message
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </main-layout>
 </template>
